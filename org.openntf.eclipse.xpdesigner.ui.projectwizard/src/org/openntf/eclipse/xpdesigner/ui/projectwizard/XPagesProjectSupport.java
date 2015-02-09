5e37e9ed8fede2297a336922312fdca7020902fd
Christian Guedemann <christian.guedemann@webgate.biz>
2015-02-09 14:31:58 +0100

    Project is now JAVA und PLUGIN Project

--- org.openntf.eclipse.xpdesigner.ui.projectwizard/src/org/openntf/eclipse/xpdesigner/ui/projectwizard/XPagesProjectSupport.java
+++ org.openntf.eclipse.xpdesigner.ui.projectwizard/src/org/openntf/eclipse/xpdesigner/ui/projectwizard/XPagesProjectSupport.java
@@ -1,6 +1,8 @@
 package org.openntf.eclipse.xpdesigner.ui.projectwizard;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFolder;
@@ -9,109 +11,173 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.environments.IExecutionEnvironment;
+import org.eclipse.jdt.launching.environments.IExecutionEnvironmentsManager;
 import org.openntf.eclipse.xpdesigner.ui.natures.ProjectNature;
- 
 
- 
 public class XPagesProjectSupport {
-    /**
-     * For this marvelous project we need to:
-     * - create the default Eclipse project
-     * - add the custom project nature
-     * - create the folder structure
-     *
-     * @param projectName
-     * @param location
-     * @param natureId
-     * @return
-     */
-    public static IProject createProject(String projectName, URI location) {
-        Assert.isNotNull(projectName);
-        Assert.isTrue(projectName.trim().length() > 0);
- 
-        IProject project = createBaseProject(projectName, location);
-        try {
-            addNature(project);
- 
-            String[] paths = { "parent/child1-1/child2", "parent/child1-2/child2/child3" }; //$NON-NLS-1$ //$NON-NLS-2$
-            addToProjectStructure(project, paths);
-        } catch (CoreException e) {
-            e.printStackTrace();
-            project = null;
-        }
- 
-        return project;
-    }
- 
-    /**
-     * Just do the basics: create a basic project.
-     *
-     * @param location
-     * @param projectName
-     */
-    private static IProject createBaseProject(String projectName, URI location) {
-        // it is acceptable to use the ResourcesPlugin class
-        IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
- 
-        if (!newProject.exists()) {
-            URI projectLocation = location;
-            IProjectDescription desc = newProject.getWorkspace().newProjectDescription(newProject.getName());
-            if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location)) {
-                projectLocation = null;
-            }
- 
-            desc.setLocationURI(projectLocation);
-            try {
-                newProject.create(desc, null);
-                if (!newProject.isOpen()) {
-                    newProject.open(null);
-                }
-            } catch (CoreException e) {
-                e.printStackTrace();
-            }
-        }
- 
-        return newProject;
-    }
- 
-    private static void createFolder(IFolder folder) throws CoreException {
-        IContainer parent = folder.getParent();
-        if (parent instanceof IFolder) {
-            createFolder((IFolder) parent);
-        }
-        if (!folder.exists()) {
-            folder.create(false, true, null);
-        }
-    }
- 
-    /**
-     * Create a folder structure with a parent root, overlay, and a few child
-     * folders.
-     *
-     * @param newProject
-     * @param paths
-     * @throws CoreException
-     */
-    private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException {
-        for (String path : paths) {
-            IFolder etcFolders = newProject.getFolder(path);
-            createFolder(etcFolders);
-        }
-    }
- 
-    private static void addNature(IProject project) throws CoreException {
-        if (!project.hasNature(ProjectNature.NATURE_ID)) {
-            IProjectDescription description = project.getDescription();
-            String[] prevNatures = description.getNatureIds();
-            String[] newNatures = new String[prevNatures.length + 1];
-            System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-            newNatures[prevNatures.length] = ProjectNature.NATURE_ID;
-            description.setNatureIds(newNatures);
- 
-            IProgressMonitor monitor = null;
-            project.setDescription(description, monitor);
-        }
-    }
- 
+	/**
+	 * For this marvelous project we need to: - create the default Eclipse
+	 * project - add the custom project nature - create the folder structure
+	 *
+	 * @param projectName
+	 * @param location
+	 * @param natureId
+	 * @return
+	 */
+	public static IProject createProject(String projectName, URI location) {
+		Assert.isNotNull(projectName);
+		Assert.isTrue(projectName.trim().length() > 0);
+
+		IProject project = createBaseProject(projectName, location);
+		try {
+			addNature(project);
+
+			String[] paths = { "Code/Java", "Forms", "Views", "CustomControls", "XPages", "WebContent/META-INF/classes", "Generated/xsp", "Generated/plugin" }; //$NON-NLS-1$ //$NON-NLS-2$
+			addToProjectStructure(project, paths);
+
+			setClasspath(project, "WebContent/META-INF/classes");
+			// BUILD PLUGIN XML
+			// BUILD ACTIVATOR
+
+		} catch (CoreException e) {
+			e.printStackTrace();
+			project = null;
+		}
+
+		return project;
+	}
+
+	/**
+	 * Just do the basics: create a basic project.
+	 *
+	 * @param location
+	 * @param projectName
+	 */
+	private static IProject createBaseProject(String projectName, URI location) {
+		// it is acceptable to use the ResourcesPlugin class
+		IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+
+		if (!newProject.exists()) {
+			URI projectLocation = location;
+			IProjectDescription desc = newProject.getWorkspace().newProjectDescription(newProject.getName());
+			if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location)) {
+				projectLocation = null;
+			}
+
+			desc.setLocationURI(projectLocation);
+			try {
+				newProject.create(desc, null);
+				if (!newProject.isOpen()) {
+					newProject.open(null);
+				}
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return newProject;
+	}
+
+	private static void createFolder(IFolder folder) throws CoreException {
+		IContainer parent = folder.getParent();
+		if (parent instanceof IFolder) {
+			createFolder((IFolder) parent);
+		}
+		if (!folder.exists()) {
+			folder.create(false, true, null);
+		}
+	}
+
+	/**
+	 * Create a folder structure with a parent root, overlay, and a few child
+	 * folders.
+	 *
+	 * @param newProject
+	 * @param paths
+	 * @throws CoreException
+	 */
+	private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException {
+		for (String path : paths) {
+			IFolder etcFolders = newProject.getFolder(path);
+			createFolder(etcFolders);
+		}
+	}
+
+	private static void addNature(IProject project) throws CoreException {
+		if (!project.hasNature(ProjectNature.NATURE_ID)) {
+			addNatureToProject(project, ProjectNature.NATURE_ID);
+		}
+		if (!project.hasNature(JavaCore.NATURE_ID)) {
+			addNatureToProject(project, JavaCore.NATURE_ID);
+		}
+		if (!project.hasNature("org.eclipse.pde.PluginNature")) {
+			addNatureToProject(project, "org.eclipse.pde.PluginNature");
+		}
+	}
+
+	private static void addNatureToProject(IProject project, String nature) throws CoreException {
+		IProjectDescription description = project.getDescription();
+		String[] prevNatures = description.getNatureIds();
+		String[] newNatures = new String[prevNatures.length + 1];
+		System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+		newNatures[prevNatures.length] = nature;
+		description.setNatureIds(newNatures);
+
+		IProgressMonitor monitor = null;
+		project.setDescription(description, monitor);
+	}
+
+	private static void setClasspath(IProject project, String outputFolder) throws JavaModelException, CoreException {
+		IJavaProject javaProject = JavaCore.create(project);
+		IPath path = project.getFullPath().append(outputFolder);
+		javaProject.setOutputLocation(path, null);
+		List<IClasspathEntry> entries = getClassPathEntries(javaProject, project);
+		javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+	}
+
+	private static List<IClasspathEntry> getClassPathEntries(IJavaProject javaProject, IProject project) {
+		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
+		entries.add(createJREEntry(null));
+		entries.add(JavaCore.newContainerEntry(new Path("org.eclipse.pde.core.requiredPlugins")));
+		IPath path = project.getProject().getFullPath();
+		entries.add(JavaCore.newSourceEntry(path.append("Java/Code")));
+		entries.add(JavaCore.newSourceEntry(path.append("Generated")));
+		return entries;
+	}
+
+	public static IClasspathEntry createJREEntry(String ee) {
+		return JavaCore.newContainerEntry(getEEPath(ee));
+	}
+
+	/**
+	 * Returns the JRE container path for the execution environment with the
+	 * given id.
+	 * 
+	 * @param ee
+	 *            execution environment id or <code>null</code>
+	 * @return JRE container path for the execution environment
+	 */
+	private static IPath getEEPath(String ee) {
+		IPath path = null;
+		if (ee != null) {
+			IExecutionEnvironmentsManager manager = JavaRuntime.getExecutionEnvironmentsManager();
+			IExecutionEnvironment env = manager.getEnvironment(ee);
+			if (env != null)
+				path = JavaRuntime.newJREContainerPath(env);
+		}
+		if (path == null) {
+			path = JavaRuntime.newDefaultJREContainerPath();
+		}
+		return path;
+	}
+
 }
