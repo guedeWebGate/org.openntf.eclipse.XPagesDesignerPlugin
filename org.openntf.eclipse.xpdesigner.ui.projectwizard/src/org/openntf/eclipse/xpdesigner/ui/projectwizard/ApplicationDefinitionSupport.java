5e7ea912f972c26b5bc9e8a266164bb6d0abd722
Christian Guedemann <christian.guedemann@webgate.biz>
2016-02-09 00:18:51 +0100

    XDPE-22 #close

--- org.openntf.eclipse.xpdesigner.ui.projectwizard/src/org/openntf/eclipse/xpdesigner/ui/projectwizard/ApplicationDefinitionSupport.java
+++ org.openntf.eclipse.xpdesigner.ui.projectwizard/src/org/openntf/eclipse/xpdesigner/ui/projectwizard/ApplicationDefinitionSupport.java
@@ -1,5 +1,9 @@
 package org.openntf.eclipse.xpdesigner.ui.projectwizard;
 
+import java.util.Properties;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Text;
@@ -7,9 +11,31 @@ import org.eclipse.swt.widgets.Text;
 public enum ApplicationDefinitionSupport {
 	INSTANCE;
 
-	public void initApplicationDefinition(Text text, Button button, Text text2, Combo combo, Combo combo2) {
-		// TODO Auto-generated method stub
-		
+	public boolean initApplicationDefinition(Text appName, Button autoupdate, Text appPath, Combo selectedServer, Combo selectedApp) {
+		IProject xpageServerProject = ServerDefinitionUtils.getXPagesServerProject(true);
+		if (xpageServerProject == null) {
+			return false;
+		}
+		String serverName = selectedServer.getText();
+		if (!ServerDefinitionUtils.folderExist(xpageServerProject, serverName, false)) {
+			return false;
+		}
+		String autoUpdateValue = autoupdate.isEnabled()?"TRUE":"FALSE";
+		String appProject = selectedApp.getText();
+		IFile file = xpageServerProject.getFile(serverName + "/"+appName.getText() +".xde-appmapping-properties");
+		Properties props = new Properties();
+		props.setProperty("SERVER_NAME", serverName);
+		props.setProperty("APPNAME", appName.getText());
+		props.setProperty("AUTOUPDATE", autoUpdateValue);
+		props.setProperty("APPPROJECT", appProject);
+		try {
+			ServerDefinitionUtils.saveProperties2File(props, file);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return true;
+
 	}
 
 }
